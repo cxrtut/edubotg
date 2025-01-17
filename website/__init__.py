bed9b742afbea939d059fa05790b9c0516bcf1db
Smangaliso-web <221568915@tut4life.ac.za>
2025-01-17 11:26:40 +0200

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,41 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
-
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize extensions
     db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+    # Register blueprints
+    from .routes import main, auth
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Note
-    
+    # Import models and create database tables
     with app.app_context():
-        db.create_all()
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+        from .models import Clients  
+        db.create_all()  
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
